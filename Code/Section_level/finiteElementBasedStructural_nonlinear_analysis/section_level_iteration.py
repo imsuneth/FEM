23f6ba2c3ab8ec74f48aa2ccf315753df905b520
Pubudu Premathilaka <pubuduudara7@gmail.com>
2019-03-14 23:31:46 +0530

    update 14/03/19

--- Code/Section_level/finiteElementBasedStructural_nonlinear_analysis/section_level_iteration.py
+++ Code/Section_level/finiteElementBasedStructural_nonlinear_analysis/section_level_iteration.py
@@ -1,7 +1,8 @@
 import numpy as np
 import math
-from Section  import *
+from Section import *
 from Element import *
+
 
 def rigidBodyTransMatrix(L):  # should return rigid body transformation matrix (L--> length of the element)
     return np.array([[0, 1 / L, 1, 0, -1 / L, 0], [0, 1 / L, 0, 0, -1 / L, 1], [-1, 0, 0, 1, 0, 0]], dtype=float)
@@ -14,8 +15,14 @@ def rotMatrix(angle):  # return element nodal deformations refering local coordi
                     [0, 0, 0, cosVal, sinVal, 0], [0, 0, 0, -sinVal, cosVal, 0],
                     [0, 0, 0, 0, 0, 1])  # should return the rotational matrix for the element
 
+
+def getElement_k():  # returns the element stiffness matrix
+    return
+
+
 def shapeFunction():
     return None
+
 
 def forceInterpolationMatrix():  # shold retrun force interpolation function to each section
     return None
@@ -37,21 +44,37 @@ def getUpdatedElemntStiffnessGlobal():
     return None
 
 
+# -----------------------------------------------------------------------------
+# assuming there is a object array for sections and elements
+elementsObjectArray = []
+sectionsObjectArry = []
 
-#-----------------------------------------------------------------------------
-#assuming there is a object array for sections and elements
-elementsObjectArray=[]
-sectionsObjectArry=[]
-#------------------------------------------------------------------------------
 
-def iteration(elementObj,updatedStrain,numOfItrPoint):
-    delta_e=np.dot(rotMatrix(elementObj.angle)*updatedStrain) # deformation increment refering local coordinate system
-    delta_e=np.dot(rigidBodyTransMatrix(elementObj.lenght)*delta_e) # transform to basic system
+# ------------------------------------------------------------------------------
 
-    elementForceIncrement=None # element force increment (what is the matrix should multiply)
+def calculateResForce():
+    pass
+
+
+def calSection_k():
+    pass
+
+
+def iteration(elementObj, updatedStrain, numOfItrPoint, maxNumOfItr):
+    delta_e = np.dot(
+        rotMatrix(elementObj.angle) * updatedStrain)  # deformation increment refering local coordinate system
+    delta_e = np.dot(rigidBodyTransMatrix(elementObj.lenght) * delta_e)  # transform to basic system
+
+    elementForceIncrement = np.dot(
+        getElement_k() * delta_e)  # element force increment (what is the matrix should multiply)
 
     for _ in numOfItrPoint:
-        delta_f_section=np.dot(shapeFunction()*elementForceIncrement)
-        #**** iteration************
+        delta_f_section = np.dot(shapeFunction() * elementForceIncrement)
+        #delta_e_section=
 
-
+        for itr in maxNumOfItr:
+            updatedSection_k = calSection_k()  # calculate section stiffness
+            res_f=calculateResForce()#calculate_resisting force
+            #newForce=calNewForce()
+            #check the tolerance
+            
