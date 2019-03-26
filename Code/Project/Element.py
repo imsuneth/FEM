8a9391415ec9776d2d4381240f82532445827e4f
imsuneth <imsuneth@gmail.com>
2019-03-26 14:11:19 +0530

    2019-03-26

--- Code/Project/Element.py
+++ Code/Project/Element.py
@@ -8,16 +8,16 @@ class Element:
 
     def __init__(self, id, start_node, end_node, element_type, n_sections):
         self.id = id
-        self.id = start_node
-        self.id = end_node
+        self.start_node = start_node
+        self.end_node = end_node
         self.element_type = element_type
         self.n_sections = n_sections
 
-        self.sections = np.array(n_sections, dtype=CrossSection)
+        self.sections = np.empty(n_sections, dtype=CrossSection)
 
     def analyze(self):
         # Pubudu, you code goes here.
         # Get inputs from Imesh as parameters of this function.
-        # Get inputs from Me using section.analyze().
+        # Get inputs from Me using section.analyze(section_deformation).
 
         return None
--- Code/Project/Material.py
+++ Code/Project/Material.py
@@ -5,5 +5,14 @@ class Material:
         self.name = name
         self.youngs_mod = youngs_mod
 
-    def getStrainFromStress(self, stress):
-        return stress * 2.3
+    def getStrainFromStress(self, material_id, stress):
+        E = 0;
+        if material_id==0:
+            fc_dash = -42800;
+            ec_dash = -0.002;
+            npop = 0.8 + (-fc_dash * 0.001 / 17);
+            E = fc_dash * ((npop - 1 + (0 / ec_dash) ^ (npop * 1)) * (npop / ec_dash) - npop * (0 / ec_dash) * (
+                    npop * 1 * (0 / ec_dash) ^ (npop * 1 - 1) * (1 / ec_dash))) / (
+                        npop - 1 + (0 / ec_dash) ^ (npop * 1)) ^ 2;
+
+        return E
