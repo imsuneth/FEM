5ae0f01dc105f8730f9a678512f499e9eb74141b
imsuneth <imsuneth@gmail.com>
2019-03-26 15:54:32 +0530

    update Section class added

--- Code/Project/CrossSection.py
+++ Code/Project/CrossSection.py
@@ -6,9 +6,6 @@ from Fiber import Fiber
 class CrossSection:
     area = 0
 
-    def analyze(self, section_deformation):
-        return None
-
 
 class SquareCrossSection(CrossSection):
 
@@ -19,35 +16,6 @@ class SquareCrossSection(CrossSection):
         self.height = height
         self.no_of_fibers = no_of_fibers
         self.fiber_material_ids = fiber_material_ids
-        self.fibers = np.empty(no_of_fibers, dtype=Fiber)
-        fiber_height = self.height / self.no_of_fibers
-
-        for fiber_id in range(no_of_fibers):
-            y = fiber_height * (self.no_of_fibers - 1 - 2 * fiber_id) / 2
-            fiber = Fiber(fiber_id, y, self.width, fiber_height, fiber_material_ids[id])
-            self.fibers.put(fiber_id, fiber)
-
-        self.analyze(self, [1, 2])
-
-    def analyze(self, section_deformation):
-        eps_0 = section_deformation[0]  # centroid strain
-        k = section_deformation[1]
-
-        A = 0
-        M = 0
-
-        print()
-
-        for fiber_id in range(self.no_of_fibers):
-            fiber = self.fibers[fiber_id]
-            eps = eps_0 - fiber.y * k
-            sigma = Material.getStrainFromStress(fiber.material_id, eps)
-            a_fib = fiber.height * fiber.width
-            a_i = sigma * a_fib
-            A += a_i
-            M += -1 * a_i * fiber.y
-
-        return None
 
 
 class CircularCrossSection(CrossSection):
--- Code/Project/Element.py
+++ Code/Project/Element.py
@@ -1,19 +1,23 @@
 import numpy as np
 from CrossSection import *
+from Section import Section
 
 
 class Element:
     angle = None
     length = None
 
-    def __init__(self, id, start_node, end_node, element_type, n_sections):
+    def __init__(self, id, start_node, end_node, cross_section, n_sections):
         self.id = id
         self.start_node = start_node
         self.end_node = end_node
-        self.element_type = element_type
+        self.cross_section = cross_section
         self.n_sections = n_sections
+        self.sections = np.empty(n_sections, dtype=Section)
 
-        self.sections = np.empty(n_sections, dtype=CrossSection)
+        for section_id in range(n_sections):
+            section = Section(section_id, cross_section)
+            self.sections.put(section_id, section)
 
     def analyze(self):
         # Pubudu, you code goes here.
