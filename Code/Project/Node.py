bab21aa273cce2a6aa6219d19a7d26d24192004b
Pubudu Premathilaka <pubuduudara7@gmail.com>
2019-03-28 02:47:32 +0530

    28/03/19

--- Code/Project/Node.py
+++ Code/Project/Node.py
@@ -5,26 +5,25 @@ class Node:
     m_x = 0
     m_y = 0
     m_z = 0
+
     d_x = 0
     d_y = 0
     d_z = 0
-    t_x = None # t for translations
+    md_x = 0
+    md_y = 0
+    md_z = 0
+
+    t_x = None  # t for translations
     t_y = None
     t_z = None
-    r_x = None # r for rotations
+    r_x = None  # r for rotations
     r_y = None
     r_z = None
 
     def __init__(self, id, p_x, p_y, p_z):
         self.id = id
-        self.p_x = p_x # p for co-ordinate
+        self.p_x = p_x  # p for co-ordinate
         self.p_y = p_y
         self.p_z = p_z
 
         return None
-
-    def getX(self):
-        return self.p_x
-
-    def getY(self):
-        return self.p_y
--- Code/Project/TestProg.py
+++ Code/Project/TestProg.py
@@ -1,16 +1,43 @@
 import numpy as np
-# x=[[1,1,0],[2,0,1]]
-# #print(x)
-# x=np.array(x,dtype=int)
+# # x=[[1,1,0],[2,0,1]]
+# # #print(x)
+# # x=np.array(x,dtype=int)
+# # print(x)
+# # print(np.transpose(x))
+# x=np.array([[1,2],[3,4]])
+# y=np.array([[5,6],[7,8]])
+# z=np.array([[9,10],[11,12]])
+# #t=np.matmul(y,x)
+# #print(t)
+# #print(np.matmul(x,y,z))
+#
+# # print(np.matmul(t,z))
+# # print(x.dot(y).dot(z))
+# # print(reduce(np.dot(x,y,z)))
 # print(x)
-# print(np.transpose(x))
-x=np.array([[1,2],[3,4]])
-y=np.array([[5,6],[7,8]])
-z=np.array([[9,10],[11,12]])
-t=np.matmul(y,x)
-print(t)
-#print(np.matmul(x,y,z))
+# np.reshape(x,4)
+# print(x)
+# a = np.array([[1],[2],[3], [4],[5],[6]])
+# print(a)
+# #np.reshape(a, 6)
+# #print(a[4])
+class Car:
+    wheels = 4
 
-# print(np.matmul(t,z))
-# print(x.dot(y).dot(z))
-# print(reduce(np.dot(x,y,z)))
+    def __init__(self, make):
+        self.make = make
+
+
+
+
+newCar = Car("Honda")
+
+newCar.wheels=5
+print (newCar.wheels)
+
+car2=Car("jaguar")
+print(car2.make)
+print(car2.wheels)
+
+car3=Car("sfaf")
+print(car3.wheels)
