fa4fd81add2e251e513e576cc3b32f16423483c8
imsuneth <imsuneth@gmail.com>
2019-03-27 10:05:22 +0530

    new updates

--- Code/Project/Material.py
+++ Code/Project/Material.py
@@ -3,21 +3,27 @@ from sympy import *
 
 class Material:
     x = Symbol('x')
+
     y_concrete = x ** 2 + 1
+    f_concrete = lambdify(x, y_concrete)
     y_concrete_prime = y_concrete.diff(x)
+    f_concrete_prime = lambdify(x, y_concrete_prime)
+
     y_steel = x ** 3 + 1
+    f_steel = lambdify(x, y_steel)
     y_steel_prime = y_steel.diff(x)
+    f_steel_prime = lambdify(x, y_steel_prime)
 
     def get_strain_from_stress(self, material_id, stress):
 
         if material_id == 0:  # concrete
-            return self.y_concrete.evalf(subs={self.x: stress})
+            return self.f_concrete(stress)
         elif material_id == 1:  # steel
-            return self.y_steel.evalf(subs={self.x: stress})
+            return self.f_steel(stress)
 
     def get_e(self, material_id, stress):
 
         if material_id == 0:  # concrete
-            return self.y_concrete_prime.evalf(subs={self.x: stress})
+            return self.f_concrete_prime(stress)
         elif material_id == 1:  # steel
-            return self.y_steel_prime.evalf(subs={self.x: stress})
+            return self.f_steel_prime(stress)
