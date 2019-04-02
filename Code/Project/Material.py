78c9553215b78f323947a5d0944deb11e7c6e3dd
Pubudu Premathilaka <pubuduudara7@gmail.com>
2019-04-02 12:14:23 +0530

    Repo Update

--- Code/Project/Material.py
+++ Code/Project/Material.py
@@ -1,9 +1,27 @@
-class Material:
+from sympy import *
 
-    def __init__(self, id, name, youngs_mod):
-        self.id = id
-        self.name = name
-        self.youngs_mod = youngs_mod
+x = Symbol('x')
 
-    def getStrainFromStress(self, stress):
-        return stress * 2.3
+y_concrete = x ** 2 + 1
+f_concrete = lambdify(x, y_concrete)
+y_concrete_prime = y_concrete.diff(x)
+f_concrete_prime = lambdify(x, y_concrete_prime)
+
+y_steel = x ** 3 + x +2
+f_steel = lambdify(x, y_steel)
+y_steel_prime = y_steel.diff(x)
+f_steel_prime = lambdify(x, y_steel_prime)
+
+
+def get_strain_from_stress(material_id, stress):
+    if material_id == 0:  # concrete
+        return f_concrete(stress)
+    elif material_id == 1:  # steel
+        return f_steel(stress)
+
+
+def get_e(material_id, stress):
+    if material_id == 0:  # concrete
+        return f_concrete_prime(stress)
+    elif material_id == 1:  # steel
+        return f_steel_prime(stress)
--- Code/Project/Structure.py
+++ Code/Project/Structure.py
@@ -135,23 +135,4 @@ class Structure:
         # Get inputs from pubudu using element.analyze()
         # passing necessary parameters to him.
 
-
-
-        ###########################################################################################
-
-        DOFcount = 0
-
-        for elementNO in Structure.n_elements:
-            numberOfFreeDOF = self.extractDOF(elementNO)
-            if numberOfFreeDOF != 0:  # not a complete fixed point %%%%%%%%%% can optimize
-
-                elementDOFdeformation = np.zeros(numberOfFreeDOF, dtype=int)
-
-                count = 1
-                while (count == numberOfFreeDOF):
-                    elementDOFdeformation.put(count - 1, Struct_def_increment[0][DOFcount])
-                    DOFcount += 1
-                    count += 1
-
-        ############################################################################################
         return None
