8dadfa9d4511665b8b0b12d8e65c0ce7a1876242
Imesh Ekanayake <imeshuek059@gmail.com>
2019-04-02 16:48:18 +0530

    Update Structure.py

--- Code/Project/Structure.py
+++ Code/Project/Structure.py
@@ -4,6 +4,7 @@ from Node import *
 from CrossSection import *
 from Material import *
 from CalculationData import *
+from numpy.linalg import inv
 
 class Structure:
     n_sections = 6
@@ -13,6 +14,7 @@ class Structure:
         # Create Node objects and put them in nparray "nodes"
 
         #self.n_totalFreeDof = 0 #added by pubudu to extractDOF from deformation increment vector
+        self.fix_Point_array=[]
 
         self.n_nodes = js["no_of_nodes"]
         self.nodes = np.empty(self.n_nodes, dtype=Node)
@@ -24,6 +26,17 @@ class Structure:
             p_z = node["z"]
             new_node = Node(id, p_x, p_y, p_z)
             self.nodes.put(id, new_node)
+
+        # Create Material objects and put them in nparray "materials"
+        self.no_of_materials = js["no_of_materials"]
+        self.materials = np.empty(self.no_of_materials, dtype=Material)
+        js_materials = js["materials"]
+        for material in js_materials:
+            id = material["id"]
+            name = material["name"]
+            youngs_mod = material["youngs_mod"]
+            new_material = Material(id, name, youngs_mod)
+            self.materials.put(id, new_material)
 
         # Create CrossSection objects and put them in nparray "cross_sections"
         self.no_of_crosssection_types = js["no_of_crosssection_types"]
@@ -70,8 +83,8 @@ class Structure:
                 angleRatio=-1*angleRatio
                 angle=math.pi-math.atan(angleRatio)
 
-            yDiff=math.abs(self.nodes[start_node_id].p_y-self.nodes[end_node_id].p_y)
-            xDiff = math.abs(self.nodes[start_node_id].p_x - self.nodes[end_node_id].p_x)
+            yDiff=abs(self.nodes[start_node_id].p_y-self.nodes[end_node_id].p_y)
+            xDiff =abs(self.nodes[start_node_id].p_x - self.nodes[end_node_id].p_x)
             length=math.sqrt(math.pow(yDiff,2)+math.pow(xDiff,2))
 
             new_element = Element(id, start_node, end_node, cross_section, self.n_sections,angle,length)
@@ -102,6 +115,7 @@ class Structure:
             # id = fixed_point["id"]
             node_id = fixed_point["point_id"]
             translation = fixed_point["translation"]
+            self.fix_Point_array+=[node_id]
             t_x = translation["x"]
             t_y = translation["y"]
             t_z = translation["z"]
@@ -131,8 +145,6 @@ class Structure:
         element=Element
         DOF = 3
 
-        Rmatrix= element.analyze(force,deformation)
-
         kGlobal=[[0 for a0 in range (DOF*3)] for a1 in range (DOF*3)]
 
         for e in range (self.n_elements):
@@ -140,37 +152,103 @@ class Structure:
             startNode=ele.start_node
             endNode=ele.end_node
             elementType=ele.element_type
-            Matrix=Rmatrix[e]
-
+            EMatrix=Rmatrix[e]
 
             y1=DOF*startNode
             y2=y1+DOF
             x1=DOF*startNode
             x2=x1+DOF
-            kGlobal[y1:y2, x1:x2] += k[:DOF_PER_NODE, :DOF_PER_NODE]
+            kGlobal[y1:y2, x1:x2] += EMatrix[:DOF, :DOF]
 
             y1 = DOF * startNode
             y2 = y1 + DOF
             x1 = DOF * endNode
             x2 = x1 + DOF
-            kGlobal[y1:y2, x1:x2] += k[:DOF_PER_NODE, DOF_PER_NODE:]
+            kGlobal[y1:y2, x1:x2] += EMatrix[:DOF, DOF:]
 
             y1=DOF*endNode
             y2=y1+DOF
             x1=DOF*startNode
             x2=x1+DOF
-            lobal[y1:y2, x1:x2] += k[DOF_PER_NODE:, :DOF_PER_NODE]
+            lobal[y1:y2, x1:x2] += EMatrix[DOF:, :DOF]
 
             y1 = DOF * endNode
             y2 = y1 + DOF
             x1 = DOF * endNode
             x2 = x1 + DOF
-            kGlobal[y1:y2, x1:x2] += k[DOF_PER_NODE:, DOF_PER_NODE:]
-
-        # Imesh, your code goes here
-        # Get inputs from pubudu using element.analyze()
-        # passing necessary parameters to him.
+            kGlobal[y1:y2, x1:x2] += EMatrix[DOF:, DOF:]
 
 
+        full_matrix=KGlobal
+        full_force=force
+        full_deformation=deformation
+        def_for_reuse=[]
+        deleting_lines=[]
 
+        for node_id in sorted(self.fix_Point_array)[::-1]:
+            node = self.nodes[node_id]
+            Line=DOF * node_id
+            if (node.r_z):
+                kGlobal = np.delete(kGlobal, Line + 2, 0)
+                kGlobal = np.delete(kGlobal, Line + 2, 1)
+                force=np.delete(force,Line + 2,0)
+                def_for_reuse=def_for_reuse+[deformation[Line + 2]]
+                deleting_lines=deleting_lines+[Line+2]
+
+
+            if (node.t_y):
+                kGlobal = np.delete(kGlobal, Line + 1, 0)
+                kGlobal = np.delete(kGlobal, Line + 1, 1)
+                force = np.delete(force, Line + 1, 0)
+                def_for_reuse = [deformation[Line + 1]] + def_for_reuse
+                deleting_lines += [Line + 1]
+
+            if (node.t_x):
+                kGlobal = np.delete(kGlobal, Line , 0)
+                kGlobal = np.delete(kGlobal, Line , 1)
+                force = np.delete(force, Line , 0)
+                def_for_reuse = [deformation[Line]] + def_for_reuse
+                deleting_lines += [Line]
+
+            deformation = inv(kGlobal)*force
+
+            for line_index in range (len(deleting_lines)):
+                np.insert(deformation, deleting_lines[line_index],def_for_reuse[line_index] , axis=0)
+
+            force= full_matrix*deformation
+
+            Rmatrix = element.analyze(force, deformation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ###########################################################################################
+
+        DOFcount = 0
+
+        for elementNO in Structure.n_elements:
+            numberOfFreeDOF = self.extractDOF(elementNO)
+            if numberOfFreeDOF != 0:  # not a complete fixed point %%%%%%%%%% can optimize
+
+                elementDOFdeformation = np.zeros(numberOfFreeDOF, dtype=int)
+
+                count = 1
+                while (count == numberOfFreeDOF):
+                    elementDOFdeformation.put(count - 1, Struct_def_increment[0][DOFcount])
+                    DOFcount += 1
+                    count += 1
+
+        ############################################################################################
         return None
