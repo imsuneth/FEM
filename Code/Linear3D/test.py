c2367ccdcc461bfdf08fd8b1aa40e77b89a5ec6c
Pubudu Premathilaka <pubuduudara7@gmail.com>
2019-05-08 13:58:46 +0530

    update 08/05/2019

--- Code/Linear3D/test.py
+++ Code/Linear3D/test.py
@@ -1,5 +1,17 @@
 import numpy as np
-x=np.cos(np.pi/3)
-y=np.sqrt(4)
-y=np.power(3,2)
-print(y)
+
+wall = np.zeros((10,10),dtype=np.int)
+block = np.array([[1,2,3],
+                  [1,2,3]])
+print(block)
+block2=np.array([[4,5,6],
+                [4,5,6]])
+print(block2)
+x = 5
+y = 3
+d=wall[x:x+block.shape[0], y:y+block.shape[1]]
+wall[x:x+block.shape[0], y:y+block.shape[1]] = block
+print(wall)
+wall[x:x+block.shape[0], y:y+block.shape[1]] =wall[x:x+block.shape[0], y:y+block.shape[1]]+ block2
+print(wall)
+print(np.zeros([12,12]))
