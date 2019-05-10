4930ba650e7502b185f41ab5a1fae97296fb1e87
imsuneth <imsuneth@gmail.com>
2019-05-10 11:43:02 +0530

    linear

--- Code/Linear3D/Main.py
+++ Code/Linear3D/Main.py
@@ -5,7 +5,8 @@ import Material
 material_js = readFile('material_models.json')
 Material.load_material_models(material_js)
 
-structure_js = readFile('testStructure.json')
+#structure_js = readFile('testStructure.json')
+structure_js = readFile('structure00.json')
 structure = Structure(structure_js)
 structure.analyzeStructure()
 
