c92db8d2140d16b537b84bc730dfd1c0e7126a85
Pubudu Premathilaka <pubuduudara7@gmail.com>
2019-06-09 03:00:10 +0530

    update before

--- Code/Project/TestProg.py
+++ Code/Project/TestProg.py
@@ -1,15 +1,5 @@
-import numpy as np
-import math
-
-class test:
-    pp=None
-    def __init__(self,var1,var2):
-        self.a=var1
-        self.b=var2
-obj1=test(2,2)
-
-print(obj1.a)
-print(obj1.pp)
-obj1.pp="adsfa"
-
-print(obj1.pp)
+x=232323
+print("hello")
+print("afsfsf")
+y=23232323
+print("asfasf")
